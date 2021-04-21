7cffd6233bb465f3b94a31d259af1e7cfa482a62
nmar30 <nmarcisso@live.com>
2021-04-21 13:05:24 -0400

    Refactoring code into methods

--- game.py
+++ game.py
@@ -7,13 +7,20 @@ class Game:
         self.player1 = self.select_player_type()
         self.player2 = self.select_player_type()
         self.win_matrix = [[-1, 1, 0, 0, 4],
-                            [1, -1, 2, 3, 1],
-                            [0, 2, -1, 2, 4],
-                            [0, 3, 2, -1, 3],
-                            [4, 1, 4, 3, -1]]
+                           [1, -1, 2, 3, 1],
+                           [0, 2, -1, 2, 4],
+                           [0, 3, 2, -1, 3],
+                           [4, 1, 4, 3, -1]]
 
     def run_game(self):
         self.same_name_fix()
+        self.battle()
+        self.determine_winner()
+
+    def welcome_message(self):
+        print('Welcome to Rock, Paper, Scissors, Lizard, Spock')
+
+    def battle(self):
         i = 0
         j = self.best_of()
         while i < j:
@@ -21,10 +28,6 @@ class Game:
             self.player2.select_gesture()
             self.compare_gestures()
             i += 1
-        self.determine_winner()
-
-    def welcome_message(self):
-        print('Welcome to Rock, Paper, Scissors, Lizard, Spock')
 
     def select_player_type(self):
         print('Please select a player type! Enter C for Computer or H for Human')
